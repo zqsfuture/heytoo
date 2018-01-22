ba8a56cd883b02c108f73f4d5aa648083e987c39
zqsfuture <421065846@qq.com>
2018-01-22 21:05:27 +0800

    4

--- js/api.js
+++ js/api.js
@@ -1,1 +1,4 @@
-var API ="http://heytoo.suzengguang.top/apply-jobs";
+var API = "http://heytoo.suzengguang.top/";
+var APPLYJOBS = API + "apply-jobs";
+var GETINFO = API + "line-activity/get-info";
+var CREATE = API + "line-activity-person/create";
--- js/index.js
+++ js/index.js
@@ -1,55 +1,134 @@
+//mui初始化禁止左滑
 mui.init({
 	swipeBack: false
 });
-mui('.mui-scroll-wrapper').scroll({
-	indicators: true //是否显示滚动条
-});
+//mui('.mui-scroll-wrapper').scroll({
+//	indicators: true //是否显示滚动条
+//});
+//轮播图
 var gallery = mui('#slider');
 gallery.slider({
 	interval: 1000 //自动轮播周期，若为0则不自动播放，默认为0；
 });
-$('.imIn').click(function() {
-	console.log("11");
-	$(".acBox").show();
-});
-$(".name").focus();
-$(".cancel").click(function() {
-	$(".acBox").hide();
-})
-$('.addJob').click(function() {
-	$(".qiuBox").show();
-});
-$(".cancel").click(function() {
-	$(".qiuBox").hide();
-})
-$('.jobInformation').click(function() {
-	$(".qiuBoxDetail").show();
-});
-$(".cancel").click(function() {
-	$(".qiuBoxDetail").hide();
-});
-$(".issue").click(function() {
-	$.ajax({
-		type: "post",
-		url: API,
-		async: true,
-		data: "",
-		success: function(res) {
-			console.log(res);
-		},
-		error: function(error) {
-			console.log(error);
-		}
+$(function() {
+	//	圈子活动
+	$('.imIn').click(function() {
+		console.log("11");
+		$(".acBox").show();
 	});
-})
-$.ajax({
-		type: "get",
-		url: API,
-		async: true,
-		success: function(res) {
-			console.log(res);
-		},
-		error: function(error) {
-			console.log(error);
-		}
+	$(".name").focus();
+	$(".cancel").click(function() {
+		$(".acBox").hide();
+	})
+	//	新增求职弹出框
+	$('.addJob').click(function() {
+		$(".qiuBox").show();
 	});
+	$(".cancel").click(function() {
+		$(".qiuBox").hide();
+	})
+	//	求职详情
+	$(".job").on("click", ".jobInformation", function(evt) {
+		$(".qiuBoxDetail").show();
+	})
+	$(".cancel").click(function() {
+		$(".qiuBoxDetail").hide();
+	});
+
+	queryQiuzhi();
+	//	获取求职列表
+	function queryQiuzhi() {
+		var str = "";
+		$.ajax({
+			type: "get",
+			url: APPLYJOBS,
+			async: true,
+			success: function(res) {
+				console.log(res);
+				$(".job").html("");
+				res.forEach(function(element) {
+					str = '<div class="jobInformation mui-clearfix">' +
+						'<div class="jobhead mui-clearfix">' +
+						'<span class="mui-pull-left">' + element.apply_job + '</span>' +
+						'<span class="mui-pull-right salary">￥' + element.apply_pay + '</span>' +
+						'</div>' +
+						'<p class="jobcont">' + element.content + '</p>' +
+						'<p>' +
+						'<span> ' + element.place + '</span>' +
+						'<span>' + element.education + '</span>' +
+						'<span>' + element.work_year + '年</span>' +
+						'<span>' + element.assume_job + '</span>' +
+						'</p>' +
+						'<div class="jobfoot mui-clearfix">' +
+						'<span class="mui-pull-left">' + element.name + '</span>' +
+						'<span class="mui-pull-right hot">鸭店推荐</span>' +
+						'</div>' +
+						'</div>';
+					$(".job").append(str);
+				});
+			},
+			error: function(error) {
+				console.log(error);
+			}
+		});
+	}
+	//	新增求职
+	$(".issue").click(function() {
+		var name = $(".qiuName").val();
+		var apply_job = $(".apply_job").val();
+		var assume_job = $(".assume_job").val();
+		var apply_pay = $(".apply_pay").val();
+		var work_year = $(".work_year").val();
+		var tel = $(".tel").val();
+		var qq = $(".qq").val();
+		var content = $(".contentIntro").val();
+		var date = {
+			"name": name,
+			"apply_job": apply_job,
+			"assume_job": assume_job,
+			'apply_pay': apply_pay,
+			"work_year": work_year,
+			"tel": tel,
+			"qq": qq,
+			"content": content
+		}
+		console.log(date);
+		$.ajax({
+			type: "post",
+			url: APPLYJOBS,
+			async: true,
+			data: date,
+			success: function(res) {
+				console.log(res);
+				$(".qiuBox").hide();
+				mui.toast('添加成功', {
+					duration: 'long',
+					type: 'div'
+				});
+				queryQiuzhi();
+			},
+			error: function(error) {
+				console.log(error);
+			}
+		});
+	})
+	
+	//	获取活动列表
+	getInfo();
+	function getInfo() {
+		$.ajax({
+			type: "get",
+			url: GETINFO,
+			success: function(res) {
+				console.log(res);
+				$(".nameTitle").html(res.name);
+				$(".time").html(res.time);
+				$(".addr").html(res.addr);
+				$(".style").html(res.style);
+			},
+			error: function(error) {
+				console.log(error);
+			}
+		});
+	}
+})
